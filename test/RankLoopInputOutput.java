6d0e30fe97d2208af43162ca9800ce8b0141bd24
master <master>
2015-06-12 18:42:02 +0800

    first commit

---
+++ test/RankLoopInputOutput.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.iterative.LoopInputOutput;
+
+public class RankLoopInputOutput implements LoopInputOutput {
+
+        @Override
+        public List<String> getLoopInputs(JobConf conf, int iteration, int step) {
+                List<String> paths = new ArrayList<String>();
+                int currentPass = iteration;
+                if (currentPass == 0) {
+                        //paths.add(conf.getOutputPath() + "/count");
+                        paths.add(conf.getInputPath());
+                }
+                paths.add("/yuzhen/tmp");
+                //if (step == 0) paths.add(conf.getInputPath());
+                
+                return paths;
+        }
+
+        @Override
+        public String getLoopOutputs(JobConf conf, int iteration, int step) {
+                int currentPass = iteration;
+                return (conf.getOutputPath() + "/i" + currentPass);
+        }
+
+}
+
